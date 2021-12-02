b346006c62c94daa2333183e2275b889ee0bc8b6
Eemeli Kouhia <e.kouhia@gmail.com>
2021-12-02 22:53:36 +0200

    Add part 1.8 unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,20 +1,12 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = ({good, neutral, bad}) => {
   const allVotes = good + neutral + bad;
-  const average = (good - bad)/(allVotes)
+  // should all votes include neutral votes when calculating average? Instructions unclear.
+  const average = (good - bad)/(allVotes) 
   const positive = good/(allVotes)
-
   return (
-    <div>
-      <h1>give feedback</h1>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+    <>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
@@ -22,6 +14,23 @@ const App = () => {
       <p>all {allVotes}</p>
       <p>average {average}</p>
       <p>positive {positive}</p>
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
