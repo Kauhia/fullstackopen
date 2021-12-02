1a5ac45a8f81d28ef7a96da1bba49aea468c2908
Eemeli Kouhia <e.kouhia@gmail.com>
2021-12-02 22:56:28 +0200

    Add part 1.9 unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -23,6 +23,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const hasVotes = !!(good + neutral + bad)
 
   return (
     <div>
@@ -30,7 +31,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      {hasVotes && <Statistics good={good} neutral={neutral} bad={bad} />}
     </div>
   )
 }
