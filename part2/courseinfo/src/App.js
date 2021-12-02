5f67b9ee487ee4194b52aface3772d7965d22023
Eemeli Kouhia <e.kouhia@gmail.com>
2021-12-03 01:57:36 +0200

    Add part 2.5 courseinfo

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,19 +1,6 @@
 import React from 'react'
+import Course from "./Course"
 
-const Header = ({course}) => <h1>{course}</h1>
-
-const Part = ({name, exercises}) => <p key={name}>{name} {exercises}</p>
-const Content = ({parts}) => parts.map(Part)
-
-const Total = ({parts}) =>
-  <p>total of {parts.reduce((a, b) => a + b.exercises, 0)} exercises</p>
-
-const Course = ({course}) => 
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts}/> 
-      <Total parts={course.parts}/>
-    </div>
 
 const App = () => {
   const courses = [
