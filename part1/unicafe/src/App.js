5f8255424c9a33f9c130a391484d6366ec70149a
Eemeli Kouhia <e.kouhia@gmail.com>
2021-12-02 23:15:57 +0200

    Add part 1.11 uniface

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,7 +2,12 @@ import React, { useState } from 'react'
 
 const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
-const StatisticLine = ({text, value}) => <p>{text} {value}</p>
+const StatisticLine = ({text, value}) =>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+
 
 const Statistics = ({good, neutral, bad}) => {
   const allVotes = good + neutral + bad;
@@ -12,12 +17,16 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <>
       <h1>statistics</h1>
-      <StatisticLine text="good" value={good}/>
-      <StatisticLine text="neutral" value={neutral}/>
-      <StatisticLine text="bad" value={bad}/>
-      <StatisticLine text="all" value={allVotes}/>
-      <StatisticLine text="average" value={average}/>
-      <StatisticLine text="positive" value={positive}/>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good}/>
+          <StatisticLine text="neutral" value={neutral}/>
+          <StatisticLine text="bad" value={bad}/>
+          <StatisticLine text="all" value={allVotes}/>
+          <StatisticLine text="average" value={average}/>
+          <StatisticLine text="positive" value={positive}/>
+        </tbody>
+      </table>
     </>
   )
 }
