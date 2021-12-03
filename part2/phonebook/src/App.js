605ac0085b25f052c9ba39ddc67fbb2a24cec4a3
Eemeli Kouhia <e.kouhia@gmail.com>
2021-12-03 16:50:16 +0200

    Add part 2.6 phonebook

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' }
+    ]) 
+    const [newName, setNewName] = useState('')
+
+    const phoneNumberOnChange = (event) => setNewName(event.target.value)
+    const onFormSubmitClick = (e) => {
+        e.preventDefault()
+        setPersons([...persons, { name: newName }])
+    }
+
+    const PhoneNumber = ({name}) => <p key={name}>{name}</p>
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form>
+                <div>
+                    name: <input value={newName} onChange={phoneNumberOnChange} />
+                </div>
+                <div>
+                    <button onClick={onFormSubmitClick} type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map(PhoneNumber)}
+        </div>
+    )
+}
+
+export default App
