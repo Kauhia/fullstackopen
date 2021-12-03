2fffb9e79ca195a3c87cee09f1923737e49c6b84
Eemeli Kouhia <e.kouhia@gmail.com>
2021-12-03 17:06:08 +0200

    Add part 2.8 phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,28 +2,34 @@ import React, { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: "020202" }
     ]) 
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
-    const phoneNumberOnChange = (event) => setNewName(event.target.value)
+    const nameOnChange = (event) => setNewName(event.target.value)
+    const phoneNumberOnChange = (event) => setNewNumber(event.target.value)
+
     const onFormSubmitClick = (e) => {
         e.preventDefault()
         if (persons.some(person => person.name === newName)) {
             alert(`${newName} is already added to phonebook`);
         } else {
-            setPersons([...persons, { name: newName }])
+            setPersons([...persons, { name: newName, number: newNumber }])
         }
     }
 
-    const PhoneNumber = ({name}) => <p key={name}>{name}</p>
+    const PhoneNumber = ({name, number}) => <p key={name}>{name}: {number}</p>
 
     return (
         <div>
             <h2>Phonebook</h2>
             <form>
                 <div>
-                    name: <input value={newName} onChange={phoneNumberOnChange} />
+                    name: <input value={newName} onChange={nameOnChange} />
+                </div>
+                <div>
+                    number: <input value={newNumber} onChange={phoneNumberOnChange} />
                 </div>
                 <div>
                     <button onClick={onFormSubmitClick} type="submit">add</button>
