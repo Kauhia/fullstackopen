0cd63214b680efabdc2001e07a922bd2b90dd941
Eemeli Kouhia <e.kouhia@gmail.com>
2022-01-17 22:58:43 +0200

    Add part3.2

--- part3/phonebook/server.js
+++ part3/phonebook/server.js
@@ -29,6 +29,18 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const peopleCount = persons.length;
+    const time = (new Date()).toString();
+    const html = `
+        <div>
+            <p>Phonebook has info for ${peopleCount} people</p>
+            <p>${time}</p>
+        </div>
+    `
+    response.send(html)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
